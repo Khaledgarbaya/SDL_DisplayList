ac6784c11bb201f9f63a2a14e4925e69bbac9dd3
Khaled Garbaya <khaledgarbaya@gmail.com>
2015-01-19 22:38:47 +0100

    Implementing Rectangle class starting point

--- geom/Rectangle.cpp
+++ geom/Rectangle.cpp
@@ -1,9 +1,70 @@
+#include <ForceFeedback/ForceFeedback.h>
+#include <iosfwd>
 #include "Rectangle.h"
 
-bool Rectangle::intersects(const Rectangle &rect) {
+bool Rectangle::intersects(const Rectangle &rect)
+{
+    return (this->x < rect.width && rect.width > rect.x &&
+            this->y < rect.height && this->height > rect.y);
+}
+
+Rectangle *Rectangle::clone() const
+{
+    return new Rectangle(*this);
+}
+
+bool Rectangle::contains(float x, float y) {
     return false;
 }
 
-Rectangle *Rectangle::clone() const {
-    return new Rectangle(*this);
+bool Rectangle::containsPoint(const Point &point) {
+    return false;
+}
+
+void Rectangle::copyFrom(const Rectangle &rect) {
+
+}
+
+bool Rectangle::equals(const Rectangle &toCompare) {
+    return false;
+}
+
+void Rectangle::inflate(float dx, float dy) {
+
+}
+
+void Rectangle::inflatePoint(const Point &point) {
+
+}
+
+Rectangle Rectangle::intersection(const Rectangle &toIntersect) const {
+    return Rectangle(0, 0, 0, 0);
+}
+
+bool Rectangle::isEmpty() {
+    return false;
+}
+
+void Rectangle::offset(float dx, float dy) {
+
+}
+
+void Rectangle::offsetPoint(const Point &point) {
+
+}
+
+void Rectangle::setEmpty() {
+
+}
+
+void Rectangle::setTo(float x, float y, float width, float height) {
+
+}
+
+std::string Rectangle::toString() {
+    return "";
+}
+
+Rectangle *Rectangle::unionWith(const Rectangle &toUnion) {
+    return nullptr;
 }
--- geom/Rectangle.h
+++ geom/Rectangle.h
@@ -1,15 +1,29 @@
 #include "Point.h"
+#include <string>
 #ifndef GEOM_RECTANGLE_H
 #define GEOM_RECTANGLE_H
 
 class Rectangle{
 public:
 
-    Rectangle(float x = 0 , float y = 0 , float width = 0, float height = 0) : x(x), y(y), width(width), height(height) {
-    }
+    Rectangle(float x = 0 , float y = 0 , float width = 0, float height = 0) : x(x), y(y), width(width), height(height) {}
+
     Rectangle* clone() const;
     bool intersects(const Rectangle &rect);
-
+    bool contains(float x, float y);
+    bool containsPoint(const Point& point);
+    void copyFrom(const Rectangle& rect);
+    bool equals(const Rectangle& toCompare);
+    void inflate(float dx, float dy);
+    void inflatePoint(const Point& point);
+    Rectangle intersection(const Rectangle& toIntersect) const;
+    bool isEmpty();
+    void offset(float dx, float dy);
+    void offsetPoint(const Point& point);
+    void setEmpty();
+    void setTo(float x, float y, float width, float height);
+    std::string toString();
+    Rectangle* unionWith(const Rectangle& toUnion);
     Point* bottomRight, topLeft;
     float top, right, bottom, left;
 
